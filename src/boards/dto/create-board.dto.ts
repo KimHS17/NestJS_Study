c88ad4f1647329620dffe79a4df5eeb84ee0190d
KimHS17 <skes01149@naver.com>
2024-03-09 22:02:46 +0900

    Pipe

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateBoardDto {
+  @IsNotEmpty() // 비어있는지 확인
   title: string;
+
+  @IsNotEmpty() // 비어있는지 확인
   description: string;
 }
